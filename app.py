8c0e21b13181e9fbfc12ab51e9799fff14242dd3
André Teixeira <andre.teixeira@gmail.com>
2016-09-27 20:28:41 -0300

    Creating base directory structure

---
+++ app.py
@@ -0,0 +1,33 @@
+import tornado.ioloop
+import tornado.web
+import os.path
+import tornado.httpserver
+
+from config import Env
+
+class Application(tornado.web.Application):
+    """
+        This is the entry point for the app.
+    """
+    def __init__(self):
+        routes = [
+        ];
+        
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "View"),
+            xsrf_cookies=True,
+            cookie_secret=Env.COOKIE_SEC,
+            debug=True,
+        )
+        
+        # Initialize the app, passing in the handlers and settings.
+        super(Application, self).__init__(routes, **settings)
+
+
+if __name__ == "__main__":
+    app = tornado.httpserver.HTTPServer(Application());
+    app.listen(8888);
+    try:
+        tornado.ioloop.IOLoop.instance().start();
+    except KeyboardInterrupt:
+        tornado.ioloop.IOLoop.instance().stop();
---
+++ config/__init__.py
@@ -0,0 +1,6 @@
+class Env:
+    COOKIE_SEC = "gl0b0c0m"
+
+    DB_HOST = '127.0.0.1'
+    DB_PORT = 127071 # MongoDB default port
+    DB_NAME = 'pytiles'
